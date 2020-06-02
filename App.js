4724ccd36b6dd4418f0c7633c98e6ca3be9821c3
apoorva-dave <apoorvadave1995@gmail.com>
2020-06-02 22:00:05 +0530

    Add scrollview

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet} from 'react-native';
 import Header from './src/components/Header'
 import PomodoroTimer from './src/components/PomodoroTimer'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <Header />
         <PomodoroTimer />
-      </View>
+      </ScrollView>
     );
   }
 }
